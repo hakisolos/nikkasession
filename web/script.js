e0f7531ababef96a24ead56a579eecab673aa015
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-12-24 17:16:36 +0100

    Update script.js

--- web/script.js
+++ web/script.js
@@ -1,9 +1,9 @@
 async function isForked(username) {
-	const apiUrl = `https://api.github.com/repos/AstroX11/Xstro/forks`;
+	const apiUrl = `https://api.github.com/repos/hakisolos/nikka-md/forks`;
 	const response = await fetch(apiUrl);
 	if (!response.ok) return false;
 
 	const forks = await response.json();
-	const filteredForks = forks.filter(fork => fork.owner.login !== 'AstroX11');
+	const filteredForks = forks.filter(fork => fork.owner.login !== 'nikka');
 	return filteredForks.some(fork => fork.owner.login === username);
 }
